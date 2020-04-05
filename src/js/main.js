b90b6b7bbffaac941795bc1242b6cf1a60c1fd2f
AnderKovalchukHome <AnderKovalchuk@gmail.com>
2020-04-05 18:42:52 +0300

    header-bug-fix

--- src/js/main.js
+++ src/js/main.js
@@ -6,13 +6,12 @@ window.addEventListener('load', () => {
 
     (function() {
         let burger  = d.querySelector('.header__mobile-burger-iner'),
-            nav     = d.querySelector('.header__nav-list'),
-            overlay = d.createElement('div');
-            overlay.className = 'overlay';
+            nav     = d.querySelector('.header__nav-list');
 
         function toggleMobmenu() {
             burger.classList.toggle('header__mobile-burger-iner--active');
             nav.classList.toggle('header__nav-list--active');
+            d.body.classList.toggle('--menu-active');
         };
 
         burger.addEventListener('click', toggleMobmenu);
